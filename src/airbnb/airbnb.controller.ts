fa37fd5ab405a6d5bb4f304303144d83b8ac6d61
Nikhil Chikane <nikhilchikane78@gmail.com>
2023-12-21 12:05:12 +0530

    airbnb server

---
+++ src/airbnb/airbnb.controller.ts
@@ -0,0 +1,56 @@
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { AirbnbService } from './airbnb.service';
+import { CreateListingDto } from 'src/dto/create_airbnbListing-dto';
+import { ListingsAndReview } from 'src/schema/airbnbschema';
+
+@Controller('airbnb')
+export class AirbnbController {
+    constructor(private airbnbService : AirbnbService){}
+
+    @Get()
+    async getAllListing(
+        @Query ('limit') limit : number
+    ): Promise<ListingsAndReview[]>{
+        return await this.airbnbService.getAllListing( limit)
+    }
+
+    @Get('/:id')
+    async getListingById(@Param('id') id : string): Promise<ListingsAndReview[]>{
+        return await this.airbnbService.getListingById(id)
+    }
+
+    @Get('/s1/search1')
+    async getListing(
+        @Query('propertyType') propertyType : string,
+        @Query('roomType') roomType : string,
+        @Query('bedrooms') bedroom : number,
+    ): Promise<ListingsAndReview[]>{
+        return await this.airbnbService.getListing(propertyType,roomType,bedroom)
+    }
+
+    @Post()
+    async createListing(
+        @Body() createListing : CreateListingDto
+    ): Promise<ListingsAndReview>{
+        return await this.airbnbService.createListing(createListing)
+    }
+
+    @Put('/update/:id')
+    async updateListing(
+        @Param('id') id : string,
+        @Body() updateListing : CreateListingDto
+    ){
+        return await this.airbnbService.updateListing(id ,updateListing)
+    }
+
+    @Delete('/delete/:id')
+    async deleteListing(
+        @Param('id') id : string
+    ): Promise<ListingsAndReview>{
+        return await this.airbnbService.deleteListing(id)
+    }
+
+    
+
+    
+}
